34b959824ee89f75126be3cd00f69c3ebfa3ab37
GutembergLima05 <125606031+GutembergLima05@users.noreply.github.com>
2023-03-07 11:46:38 -0300

    C# POO

---
+++ TestePOO2/TestePOO2/Funcionario.cs
@@ -0,0 +1,9 @@
+﻿
+namespace TestePOO2
+{
+    internal class Funcionario
+    {
+        public string Nome;
+        public double Salario;
+    }
+}
---
+++ TestePOO2/TestePOO2/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Globalization;
+using TestePOO2;
+
+namespace Course
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int media;
+            Funcionario f1 = new Funcionario();
+            Funcionario f2 = new Funcionario();
+
+            Console.WriteLine("Dados do primeiro funcionário: ");
+            Console.Write("Nome: ");
+            f1.Nome = Console.ReadLine();
+            Console.Write("Salario: ");
+            f1.Salario = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Dados do segundo funcionário: ");
+            Console.Write("Nome: ");
+            f2.Nome = Console.ReadLine();
+            Console.Write("Salario: ");
+            f2.Salario = double.Parse(Console.ReadLine());
+        }
+    }
+}
