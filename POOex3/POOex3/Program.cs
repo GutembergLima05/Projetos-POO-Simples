cad855c03ad914581d4ad70b0f4fd53c36c4e634
Gutemberg Lima <125606031+GutembergLima05@users.noreply.github.com>
2023-03-10 14:38:58 -0300

    C# POO

---
+++ POOex3/POOex3/Program.cs
@@ -0,0 +1,33 @@
+﻿using POOex3;
+using System;
+using System.Globalization;
+
+namespace Course
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Aluno aluno = new Aluno();
+
+            Console.Write("Nome do aluno: ");
+            aluno.Nome = Console.ReadLine();
+
+            Console.WriteLine("Digite as três notas do aluno:");
+            aluno.nota1 = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            aluno.nota2 = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            aluno.nota3 = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Console.WriteLine("NOTA FINAL = " + aluno.NotaFinal().ToString("F2", CultureInfo.InvariantCulture));
+            
+            if (aluno.NotaFinal() >= 60.00)
+            {
+                Console.WriteLine("APROVADO");
+            } else
+            {
+                Console.WriteLine("REPROVADO");
+                Console.WriteLine("FALTARAM " + (60.00 - aluno.NotaFinal()).ToString("F2", CultureInfo.InvariantCulture) + " PONTOS");
+            }
+        }
+    }
+}
