6ad08678a674ce828e9a00d00fa38ef976803b18
Gutemberg Lima <125606031+GutembergLima05@users.noreply.github.com>
2023-03-10 14:38:22 -0300

    C# POO

---
+++ POOex2/POOex2/Fucionario.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace POOex2
+{
+    internal class Fucionario
+    {
+        public string Nome;
+        public double SalarioBruto;
+        public double Imposto;
+
+        public double SalarioLiquido()
+        {
+            return SalarioBruto - Imposto;
+        }
+
+        public void AumentarSalario(double porcentagem)
+        {
+            
+            SalarioBruto += SalarioBruto / 100 * porcentagem;
+            
+        }
+
+        public override string ToString()
+        {
+            return
+                Nome + ", $ " + SalarioLiquido().ToString("F2", CultureInfo.InvariantCulture); 
+        }
+    }
+}
---
+++ POOex2/POOex2/Program.cs
@@ -0,0 +1,37 @@
+﻿using POOex2;
+using System;
+using System.Globalization;
+
+namespace Course
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Fucionario f;
+            f = new Fucionario();
+
+            Console.Write("Nome: ");
+            f.Nome = Console.ReadLine();
+
+            Console.Write("Salário bruto: ");
+            f.SalarioBruto = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Console.Write("Imposto: ");
+            f.Imposto = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Console.WriteLine();
+
+            Console.WriteLine("Funcionário: " + f);
+
+            Console.WriteLine();
+
+            Console.WriteLine("Digite a porcentagem para aumentar o salário: ");
+            double aumento = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            f.AumentarSalario(aumento);
+            Console.WriteLine();
+
+            Console.WriteLine("Dados atualizados: " + f);
+        }
+    }
+}
