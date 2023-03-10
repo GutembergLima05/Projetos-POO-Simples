74afb7179547b304cfc97b5689c2244e8513b8e9
Gutemberg Lima <125606031+GutembergLima05@users.noreply.github.com>
2023-03-10 14:37:43 -0300

    C# POO

---
+++ POOex/POOex/Program.cs
@@ -0,0 +1,26 @@
+﻿using POOex;
+using System;
+using System.Globalization;
+
+namespace Course
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Retangulo x;
+                x = new Retangulo();
+
+            Console.WriteLine("Entre a largura e altura do retângulo:");
+            x.Largura = double.Parse(Console.ReadLine(),CultureInfo.InvariantCulture);
+            x.Altura = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Console.WriteLine("AREA = " + x.Area().ToString("F2", CultureInfo.InvariantCulture));
+            Console.WriteLine("PERÍMETRO = " + x.Perimetro().ToString("F2", CultureInfo.InvariantCulture));
+            Console.WriteLine("DIAGONAL = " + x.Diagonal().ToString("F2", CultureInfo.InvariantCulture));
+
+
+
+        }
+    }
+}
---
+++ POOex/POOex/Retangulo.cs
@@ -0,0 +1,24 @@
+﻿using System.Globalization;
+
+namespace POOex
+{
+    internal class Retangulo
+    {
+        public double Altura, Largura;
+
+        public double Area()
+        {
+            return  Altura * Largura;
+        }
+
+        public double Perimetro()
+        {
+           return 2 * Altura + 2 * Largura;
+        }
+
+        public double Diagonal()
+        {
+            return Math.Sqrt(Math.Pow(Altura, 2) + Math.Pow(Largura, 2));
+        }
+    }
+}
