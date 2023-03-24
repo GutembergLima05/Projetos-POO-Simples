430cad3e9b8823ea53c5e63c85b47b482189e448
Gutemberg Lima <j.berglima05@gmail.com>
2023-03-24 16:40:38 -0300

    C# POO

---
+++ Enum/Enum/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Enum.Entities;
+using Enum.Entities.Enums;
+
+namespace Enum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Order order = new Order
+            {
+                Id = 1080,
+                Moment = DateTime.Now,
+                Status = OrderStatus.PedingPayment
+            };
+
+            Console.WriteLine(order);
+
+            string txt = OrderStatus.PedingPayment.ToString();
+
+            Console.WriteLine(txt);
+
+        }
+    }
+}
