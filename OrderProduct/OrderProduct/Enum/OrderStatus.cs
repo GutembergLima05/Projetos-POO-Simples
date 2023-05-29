11dd35a341d764fa92e166f4eb98d6c0200bdff0
Gutemberg Lima <j.berglima05@gmail.com>
2023-05-29 11:23:10 -0300

    C# POO

---
+++ OrderProduct/OrderProduct/Enum/OrderStatus.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace OrderProduct.Enum
+{
+    internal enum OrderStatus : int
+    {
+        PendingPayment = 0,
+        Processing = 1,
+        Shipped = 2,
+        Delivered = 3
+
+    }
+}
