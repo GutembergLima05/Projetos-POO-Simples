4c5ca040b707df67783520a58cc21dc6f8328786
Gutemberg Lima <j.berglima05@gmail.com>
2023-06-06 21:28:19 -0300

    C# POO

---
+++ ProductIT/ProductIT/Program.cs
@@ -0,0 +1,50 @@
+﻿using ProductIT.Entities;
+using System.Diagnostics;
+using System.Globalization;
+using System.Xml.Linq;
+
+namespace Course
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Product> list = new List<Product>();
+            Console.Write("Enter the number of products: ");
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i <= n; i++)
+            {
+                Console.WriteLine($"Product #{i} data:");
+                Console.Write("Common, used or imported (c/u/i)? ");
+                string ch = Console.ReadLine();
+                Console.Write("Name: ");
+                string name = Console.ReadLine();
+                Console.Write("Price: ");
+                double price = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+                if (ch == "i")
+                {
+                    Console.Write("Customs fee: ");
+                    double customsfee = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                    list.Add(new ImportedProduct(name, price, customsfee));
+                }
+                else if (ch == "u")
+                {
+                    Console.Write("Manufacture date (DD/MM/YYYY): ");
+                    DateTime manuFactureDate = DateTime.Parse(Console.ReadLine());
+                    list.Add(new UsedProduct(name, price, manuFactureDate));
+                } else
+                {
+                    list.Add(new Product(name, price));
+                }
+            }
+            Console.WriteLine();
+            Console.WriteLine("PRICE TAGS:");
+            foreach (Product product in list)
+            {
+                Console.WriteLine(product.PriceTag());
+            }
+        }
+    }
+}
